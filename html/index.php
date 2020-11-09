1ed265e3ca859d31b99ad892fbb63db5791e40fb
AlexisMejia <00090718@uca.edu.sv>
2020-11-09 16:14:19 -0600

    Html: done

--- html/index.php
+++ html/index.php
@@ -1,58 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/indexStyle.css" rel="stylesheet" type="text/css">
     <title>Principal View</title>
 </head>
 <?php include("../php/db_cnt.php"); ?>
+
 <body>
-    <table>
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Passport</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Citizen</th>
-            <th>Phone number</th>
-            <th>Country from</th>
-            <th>Country to</th>
-            <th>Date</th>
-            <th>Flight time</th>
-            <th>Seat</th>
-            <th>Price</th>
-            <th>Class</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <thbody>
-    <?php
+    <a href="createForm.php">
+        <input type="button" name="save" value="Create" />
+    </a>
+    <table id="airportID" width="100%">
+        <thead>
+            <tr>
+                <th id="title">Id</th>
+                <th id="title">Passport</th>
+                <th id="title">Name</th>
+                <th id="title">Age</th>
+                <th id="title">Citizen</th>
+                <th id="title">Phone number</th>
+                <th id="title">Country from</th>
+                <th id="title">Country to</th>
+                <th id="title">Date</th>
+                <th id="title">Flight time</th>
+                <th id="title">Seat</th>
+                <th id="title">Price</th>
+                <th id="title">Class</th>
+                <th id="title">Action</th>
+            </tr>
+        </thead>
+        <thbody>
+            <?php
         $query = "SELECT * FROM form2";
         $result_tasks = mysqli_query($conex, $query);    
         while($row = mysqli_fetch_assoc($result_tasks)) { ?>
-        <tr>
-        <td><?php echo $row['id']; ?></td>
-        <td><?php echo $row['passp_user']; ?></td>
-        <td><?php echo $row['name_user']; ?></td>
-        <td><?php echo $row['age_user']; ?><td>
-        <td><?php echo $row['citi_user']; ?></td>
-        <td><?php echo $row['phone_user']; ?></td>
-        <td><?php echo $row['pais_origen']; ?></td>
-        <td><?php echo $row['pais_llegada']; ?></td>
-        <td><?php echo $row['fecha_reg']; ?></td>
-        <td><?php echo $row['hora_vuelo']; ?></td>
-        <td><?php echo $row['asiento']; ?></td>
-        <td><?php echo $row['precio']; ?></td>
-        <td><?php echo $row['clase']; ?></td>
-        <td>
-        <a href="delete.php?id=<?php echo $row['id']?>">
-        <input type="submit" name="registerF" value="Delete"/>
-        </a>
-        </td>
-        </tr>
-        <?php } ?>
-    </thbody>
+            <tr>
+                <td class="info"><?php echo $row['id']; ?></td>
+                <td class="info"><?php echo $row['passp_user']; ?></td>
+                <td class="info"><?php echo $row['name_user']; ?></td>
+                <td class="info"><?php echo $row['age_user']; ?></td>
+                <td class="info"><?php echo $row['citi_user']; ?></td>
+                <td class="info"><?php echo $row['phone_user']; ?></td>
+                <td class="info"><?php echo $row['pais_origen']; ?></td>
+                <td class="info"><?php echo $row['pais_llegada']; ?></td>
+                <td class="info"><?php echo $row['fecha_reg']; ?></td>
+                <td class="info"><?php echo $row['hora_vuelo']; ?></td>
+                <td class="info"><?php echo $row['asiento']; ?></td>
+                <td class="info"><?php echo $row['precio']; ?></td>
+                <td class="info"><?php echo $row['clase']; ?></td>
+                <td class="info">
+                    <a href="../php/edit.php?id=<?php echo $row['id']?>">
+                        <input type="submit" name="edit" value="Edit" />
+                    </a>
+                    <a href="../php/delete.php?id=<?php echo $row['id']?>">
+                        <input type="submit" name="delete" value="Delete" />
+                    </a>
+                </td>
+            </tr>
+            <?php } ?>
+        </thbody>
     </table>
 </body>
+
 </html>
