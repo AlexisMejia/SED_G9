b23d8dc4d23c4e74617d29cace04b69ed25e304a
AlexisMejia <00090718@uca.edu.sv>
2020-11-14 02:52:38 -0600

    PHP: Hashing password done, still working on login

--- php/hack.php
+++ php/hack.php
@@ -1,38 +1,38 @@
 <?php
 
-function encode($param){
-    $paramencode = $param;
-    $num = mt_rand(5,5);
+include("db_cnt.php");
 
-    for ($i=0; $i <$num ; $i++) { 
-        $paramencode = base64_encode($paramencode);
-    }
+//saving data from register to database
+if (isset($_POST['register'])) {
+    if (strlen($_POST['user']) >= 1 && strlen($_POST['password']) >= 1) {
+        $user = trim($_POST['user']);
+        $pwd = trim($_POST['password']);
+        $cont = 0;
+        $pssw_cifrada = password_hash($pwd, PASSWORD_DEFAULT, );
+        $consulta = "INSERT INTO login(user, pssw) VALUES ('$user','$pssw_cifrada')";
+        $resultado = mysqli_query($conex,$consulta);
 
-    $primary_array = array('F','I','A','C','T','D','O','V','N','O','A','B');
-    $paramencode = $paramencode."+".$primary_array[$num];
-    $paramencode = base64_encode($paramencode);
+        while($pass = $resultado->dba_fetch(PDO::dba_fetch)){
+            if(password_verify($pwd, $pass['pwd'])){
+                $cont++;
+            }
+        }
 
-    return $paramencode;
+        if($cont>0){
+            ?> 
+            echo "<script>
+                        alert('Welcome!');
+                        window.location= 'index.php'
+                </script>";
+            <?php
+        }else{
+            ?>  
+            echo "<script>
+                        alert('Ups,something went wrong!');
+                        window.location= 'loginForm.php'
+                </script>";
+            <?php
+        }
 }
-function decoded($str) 
-{ 
-    $alpha_array = 
-    array('Y','D','U','R','P', 
-    'S','B','M','A','T','H'); 
-    $decoded = 
-    base64_decode($str); 
-    list($decoded,$letter) = 
-    split("\+",$decoded); 
-    for($i=0;$i<count($alpha_array);$i++) 
-    { 
-    if($alpha_array[$i] == $letter) 
-    break; 
-    } 
-    for($j=1;$j<=$i;$j++) 
-    { 
-        $decoded = 
-        base64_decode($decoded); 
-    } 
-    return $decoded; 
 }
 ?>
--- php/register.php
+++ php/register.php
@@ -6,21 +6,31 @@ if (isset($_POST['register'])) {
     if (strlen($_POST['user']) >= 1 && strlen($_POST['password']) >= 1) {
         $user = trim($_POST['user']);
         $pwd = trim($_POST['password']);
-        $consulta = "INSERT INTO register(usuario, contra) VALUES ('$user','$pwd')";
+        $pssw_cifrada = password_hash($pwd, PASSWORD_DEFAULT, );
+        $consulta = "INSERT INTO login(user, pssw) VALUES ('$user','$pssw_cifrada')";
         $resultado = mysqli_query($conex,$consulta);
         if ($resultado) {
             ?> 
-            <h3 class="ok">¡Te has inscrito correctamente!</h3>
+            echo "<script>
+                        alert('Welcome!');
+                        window.location= 'index.php'
+                </script>";
             <?php
         } else {
-            ?> 
-            <h3 class="bad">¡Ups ha ocurrido un error!</h3>
+            ?>  
+            echo "<script>
+                        alert('Ups,something went wrong!');
+                        window.location= 'loginForm.php'
+                </script>";
             <?php
         }
     }   else {
-            ?> 
-            <h3 class="bad">¡Por favor complete los campos!</h3>
-            <?php
+        ?>  
+        echo "<script>
+                    alert('Please fill in all fields!');
+                    window.location= 'loginForm.php'
+            </script>";
+        <?php
     }
 }
 
