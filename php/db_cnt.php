55d4a302b410ce07873ac51b617432e2f43c1940
AlexisMejia <00090718@uca.edu.sv>
2020-11-19 16:28:18 -0600

    PHP: .env done

--- php/db_cnt.php
+++ php/db_cnt.php
@@ -1,9 +1,13 @@
 <?php
 
-$servername = "localhost";
-$database = "sed";
-$username = "root";
-$password = "";
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];;
+$database = $_ENV['DB_NAME'];;
+$username = $_ENV['DB_USER'];;
+$password = $_ENV['DB_PASS'];;
 // Create connection
 $conex = mysqli_connect($servername, $username, $password, $database);
 // Check connection
